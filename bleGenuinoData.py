b9676cf88b9e7005e32725ea01133ab16def20fa
root <root@tx2.lan>
2017-11-03 00:45:33 +0000

    Buffer size 10

--- bleGenuinoData.py
+++ bleGenuinoData.py
@@ -1,9 +1,13 @@
 import time
 from gattlib import GATTRequester
+import numpy as np
 
 req = GATTRequester("98:4f:ee:10:d4:90")
 
+bufferSize = 10
 while True:
-	data = req.read_by_uuid("3a19")
+	data = [0] * bufferSize
+	for i in range(bufferSize):
+		data[i] = req.read_by_uuid("3a19")
 	print data
-	time.sleep(0.005)
+	time.sleep(0.05)
