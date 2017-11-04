d351218f9c7b39296dfafd46710dc9d026c3d506
temp3rr0r <madks@hotmail.com>
2017-11-04 19:46:00 +0000

    Before unifying ble & publish aws

--- README.md
+++ README.md
@@ -5,13 +5,13 @@
 Packages:
 ```
 apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
-apt-get install python3 python3-pip glib-2.0-dev libbluetooth-dev libreadline-dev libboost-python-dev pkg-config libboost-python-dev libboost-thread-dev libglib2.0-dev python-dev
+apt-get install python python-pip glib-2.0-dev libbluetooth-dev libreadline-dev libboost-python-dev pkg-config libboost-python-dev libboost-thread-dev libglib2.0-dev python-dev
 ```
 
 Pip:
 ```
-pip3 install AWSIoTPythonSDK
-pip3 install gattlib
+pip install AWSIoTPythonSDK
+pip install gattlib
 ```
 
 Source code samples
---
+++ basicPubSub.py
@@ -0,0 +1,104 @@
+import time
+from gattlib import GATTRequester
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                    help="Targeted client id")
+parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+useWebsocket = args.useWebsocket
+clientId = args.clientId
+topic = args.topic
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, 443)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+time.sleep(2)
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+publishDelay = 1 # seconds
+bleDelay = 0.05
+bufferSize = 1
+
+try:
+	req = GATTRequester("98:4f:ee:10:d4:90") # BLE genuino 101 address
+
+	while True:
+		
+		data = [0] * bufferSize # Init buffer
+	        for i in range(bufferSize):
+        	        data[i] = req.read_by_uuid("3a19") # Read IMU data
+
+		print data
+		msg = 'ax: 0.3, ay: 0.2, az: 0.25, gx: 1, gy: 2, gz: 3';
+		msg = '{' + msg  + '}'
+		myAWSIoTMQTTClient.publish(topic, msg, 1)
+		loopCount += 1
+		time.sleep(publishDelay)
+except KeyboardInterrupt:
+	pass
+
+print('Exiting the loop');
+myAWSIoTMQTTClient.disconnect()
+print('Disconnected from AWS')
