ed84f66671fd4d5431b0eff07e635dd23558b43f
temp3rr0r <madks@hotmail.com>
2017-11-04 20:26:26 +0000

    Can read many buffer messages

--- bleGenuinoData.py
+++ bleGenuinoData.py
@@ -4,35 +4,15 @@ from gattlib import GATTRequester
 
 req = GATTRequester("98:4f:ee:10:d4:90")
 
-bufferSize = 1
+bufferSize = 4
 while True:
 	data = [0] * bufferSize
 	for i in range(bufferSize):
 		data[i] = req.read_by_uuid("3a19")[0]
 
-	print "data"
-	print str(data)
-	print ""
-
-	print "data[0]"
-	print str(data[0])
-	print ""
-
-	print "data[0][0]"
-	print str(data[0][0])
-	print ""	
-
-
-	for i in range(0, 6):
-		floatValue = struct.unpack_from('f', data[0], 2 * i)
-		print "float {0}: {1}".format(i, floatValue)
-
-        print("bytes received:")
-       # for b in data[0]:
-        #    print str(b)
-        #    print ", "
-        #    #print ord(b)
-        #    print ", "
-        #print("")
+	for j in range(0, bufferSize):
+		for i in range(0, 6):
+			floatValue = struct.unpack_from('f', data[j], 2 * i)
+			print "Buffer {0}, {1}: {2}".format(j, i, floatValue)
 
 	time.sleep(1.05)
